cfb6c07b62df39cecb28e5ad083fecfafc888aa0
Chris Patton <cd.patton@gmail.com>
2023-05-05 21:19:46 -0700

    car plugin

--- examples/car/main.rs
+++ examples/car/main.rs
@@ -6,23 +6,11 @@ mod camera_az_el;
 mod control;
 mod create_car_json;
 mod environment;
-pub mod physics;
+mod physics;
+mod plugin;
 mod schedule;
 
-use bevy_integrator::{
-    integrator::{initialize_state, integrator_schedule, PhysicsSchedule, Solver},
-    recorder::{create_recorder, initialize_recorder, load_recorded_data, recorder_system},
-};
-use bevy_rigid_body::{
-    joint::{bevy_joint_positions, Joint},
-    structure::loop_1,
-};
-use camera_az_el::camera_builder;
-
-use control::CarControl;
-use create_car_json::car_json;
-use environment::build_environment;
-use schedule::{create_physics_schedule, set_replay_data};
+use plugin::{CarPlugin, Mode};
 
 // set a larger timestep if the animation lags
 const FIXED_TIMESTEP: f32 = 0.002; // 0.002 -> 500 fps
@@ -30,77 +18,16 @@ const FIXED_TIMESTEP: f32 = 0.002; // 0.002 -> 500 fps
 // Main function
 fn main() {
     // Create App
-    let mut app = App::new();
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            resolution: (1200., 900.).into(),
-            title: "Car Demo".to_string(),
-            resizable: true,
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (1200., 900.).into(),
+                title: "Car Demo".to_string(),
+                resizable: true,
+                ..default()
+            }),
             ..default()
-        }),
-        ..default()
-    }))
-    .add_startup_system(camera_builder(
-        Vec3 {
-            x: 0.,
-            y: 10.,
-            z: 1.,
-        },
-        0.0_f32.to_radians(),
-        10.0_f32.to_radians(),
-        20.,
-        camera_az_el::UpDirection::Z,
-    ))
-    .add_system(camera_az_el::az_el_camera) // setup the camera
-    .add_startup_system(setup_system) // setup the car model and environment
-    .add_startup_systems(
-        (initialize_state::<Joint>,)
-            .chain()
-            .in_base_set(StartupSet::PostStartup),
-    )
-    .add_system(bevy_joint_positions); // update the bevy joint positions
-
-    // data recording
-    if false {
-        app.add_startup_system(create_recorder)
-            .add_startup_system(initialize_recorder::<Joint>.in_base_set(StartupSet::PostStartup))
-            .add_system(recorder_system::<Joint>);
-    }
-
-    if true {
-        // run the physics simulation with user control
-        let schedule = create_physics_schedule();
-        app.add_schedule(PhysicsSchedule, schedule) // add the physics schedule
-            .insert_resource(Solver::RK4) // set the solver to use
-            .insert_resource(FixedTime::new_from_secs(FIXED_TIMESTEP)) // set the fixed timestep
-            .add_system(integrator_schedule::<Joint>.in_schedule(CoreSchedule::FixedUpdate)) // run the physics schedule in the fixed timestep loop
-            .add_system(control::user_control_system) // control the car with a gamepad
-            .init_resource::<CarControl>();
-    } else {
-        // replay recorded data
-        app.add_startup_system(load_recorded_data).add_systems(
-            (
-                set_replay_data, // sets the joint position data
-                loop_1,          // calculates joint transforms
-            )
-                .chain(),
-        );
-    }
-
-    // Run the app
-    app.run();
-}
-
-pub fn setup_system(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    if true {
-        car_json();
-        build_from_json::build_model(&mut commands, &mut meshes, &mut materials);
-    } else {
-        build::build_model(&mut commands, &mut meshes, &mut materials);
-    }
-    build_environment(&mut commands, &mut meshes, &mut materials);
+        }))
+        .add_plugin(CarPlugin { mode: Mode::None })
+        .run(); // update the bevy joint positions
 }
---
+++ examples/car/plugin.rs
@@ -0,0 +1,106 @@
+use bevy::prelude::*;
+use bevy_integrator::{
+    integrator::{initialize_state, integrator_schedule, PhysicsSchedule, Solver},
+    recorder::{create_recorder, initialize_recorder, load_recorded_data, recorder_system},
+};
+use bevy_rigid_body::{
+    joint::{bevy_joint_positions, Joint},
+    structure::loop_1,
+};
+
+use crate::{
+    build, build_from_json,
+    camera_az_el::{self, camera_builder},
+    control::{self, CarControl},
+    create_car_json::car_json,
+    environment::build_environment,
+    schedule::{create_physics_schedule, set_replay_data},
+    FIXED_TIMESTEP,
+};
+
+#[derive(Default)]
+pub enum Mode {
+    Record,
+    Playback,
+    #[default]
+    None,
+}
+
+pub struct CarPlugin {
+    pub(crate) mode: Mode,
+}
+
+impl CarPlugin {
+    pub fn setup_physics_simulation(&self, app: &mut App) {
+        // run the physics simulation with user control
+        let schedule = create_physics_schedule();
+        app.add_schedule(PhysicsSchedule, schedule) // add the physics schedule
+            .insert_resource(Solver::RK4) // set the solver to use
+            .insert_resource(FixedTime::new_from_secs(FIXED_TIMESTEP)) // set the fixed timestep
+            .add_system(integrator_schedule::<Joint>.in_schedule(CoreSchedule::FixedUpdate)) // run the physics schedule in the fixed timestep loop
+            .add_system(control::user_control_system) // control the car with a gamepad
+            .init_resource::<CarControl>();
+    }
+}
+
+impl Plugin for CarPlugin {
+    fn build(&self, app: &mut App) {
+        match self.mode {
+            Mode::Record => {
+                app.add_startup_system(create_recorder)
+                    .add_startup_system(
+                        initialize_recorder::<Joint>.in_base_set(StartupSet::PostStartup),
+                    )
+                    .add_system(recorder_system::<Joint>);
+                self.setup_physics_simulation(app)
+            }
+            Mode::Playback => {
+                app.add_startup_system(load_recorded_data).add_systems(
+                    (
+                        set_replay_data, // sets the joint position data
+                        loop_1,          // calculates joint transforms
+                    )
+                        .chain(),
+                );
+            }
+            Mode::None => self.setup_physics_simulation(app),
+        }
+
+        // camera setup
+        app.add_startup_system(camera_builder(
+            Vec3 {
+                x: 0.,
+                y: 10.,
+                z: 1.,
+            },
+            0.0_f32.to_radians(),
+            10.0_f32.to_radians(),
+            20.,
+            camera_az_el::UpDirection::Z,
+        ))
+        .add_system(camera_az_el::az_el_camera); // setup the camera
+
+        // car setup
+        app.add_startup_system(setup_system) // setup the car model and environment
+            .add_startup_systems(
+                (initialize_state::<Joint>,)
+                    .chain()
+                    .in_base_set(StartupSet::PostStartup),
+            )
+            .add_system(bevy_joint_positions);
+    }
+}
+
+pub fn setup_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    if true {
+        car_json();
+        build_from_json::build_model(&mut commands, &mut meshes, &mut materials);
+    } else {
+        build::build_model(&mut commands, &mut meshes, &mut materials);
+    }
+    build_environment(&mut commands, &mut meshes, &mut materials);
+}
